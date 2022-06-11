087cb66deae1d4c56c246d45f4f4845bd1d64edd
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-06-12 01:06:28 +0300

    Fix tests

--- test/test_mocked_warper.cpp
+++ test/test_mocked_warper.cpp
@@ -22,54 +22,53 @@ int16_t CallMock(vva::IOperationWarper& warper, Func func, Args... args) {
 
 namespace vva {
 struct MockedWarperTest {
-  fakeit::Mock<IOperationWarper> mockWarper_;
+  std::unique_ptr<fakeit::Mock<IOperationWarper>> mockWarper_;
 };
 
 UTEST_F_SETUP(MockedWarperTest) {
-  static_cast<void>(utest_fixture->mockWarper_);
+  utest_fixture->mockWarper_ = std::make_unique<fakeit::Mock<IOperationWarper>>();
 }
 
 UTEST_F_TEARDOWN(MockedWarperTest) {
-  static_cast<void>(utest_fixture->mockWarper_);
 }
 
 UTEST_F(MockedWarperTest, MockedWarperTestAdd_OneToTwo_Three) {
-  When(Method(utest_fixture->mockWarper_, addition).Using(1, 2)).Return(3);
+  When(Method(*(utest_fixture->mockWarper_), addition).Using(1, 2)).Return(3);
 
-  EXPECT_EQ(CallMock(utest_fixture->mockWarper_.get(),
+  EXPECT_EQ(CallMock((utest_fixture->mockWarper_)->get(),
                      &IOperationWarper::addition, 1, 2),
             3);
 }
 
 // This test intentionally produces wrong result
 UTEST_F(MockedWarperTest, MockedWarperTestAdd_OneToTwo_Four) {
-  When(Method(utest_fixture->mockWarper_, addition)).Return(4);
+  When(Method(*(utest_fixture->mockWarper_), addition)).Return(4);
 
-  EXPECT_EQ(CallMock(mockWarper_->mockWarper.get(), &IOperationWarper::addition,
+  EXPECT_EQ(CallMock((utest_fixture->mockWarper_)->get(), &IOperationWarper::addition,
                      1, 2),
             4);
 }
 
-// UTEST_F(MockedWarperTest,
-//                  MockedWarperTestAdd_SignedIntOverflowException) {
-//   constexpr auto a = kMaxValue;
-//   constexpr int16_t b = 1;
+UTEST_F(MockedWarperTest,
+                 MockedWarperTestAdd_SignedIntOverflowException) {
+  constexpr auto a = kMaxValue;
+  constexpr int16_t b = 1;
 
-When(Method(mockWarper_, addition).Using(a, b))
+When(Method(*(utest_fixture->mockWarper_), addition).Using(a, b))
     .Throw(std::overflow_error("REQUIRE_THROWS_MATCHES"));
 
-REQUIRE_THROWS_AS(CallMock(mockWarper_.get(), &IOperationWarper::addition, a,
+EXPECT_EXCEPTION(CallMock((utest_fixture->mockWarper_)->get(), &IOperationWarper::addition, a,
                            b),
                   std::overflow_error);
-}  // namespace vva
+}
 
 UTEST_F(MockedWarperTest, MockedWarperTestAdd_SignedIntOverflowClamped) {
   constexpr auto a = kMaxValue;
   constexpr int16_t b = 1;
 
-  When(Method(utest_fixture->mockWarper_, addition).Using(a, b)).Return(a);
+  When(Method(*(utest_fixture->mockWarper_), addition).Using(a, b)).Return(a);
 
-  EXPECT_EQ(CallMock(utest_fixture->mockWarper_.get(),
+  EXPECT_EQ(CallMock((utest_fixture->mockWarper_)->get(),
                      &IOperationWarper::addition, a, b),
             a);
 }
--- test/test_operation_strategy_logic.cpp
+++ test/test_operation_strategy_logic.cpp
@@ -1,9 +1,10 @@
-#include <catch2/catch.hpp>
+#include <utest/utest.h>
 #include <cstdint>
 #include <fakeit/fakeit.hpp>
 #include <i_warper.hpp>
 #include <limits>
 #include <operation_strategy.hpp>
+#include <memory>
 
 namespace {
 using fakeit::Mock;
@@ -12,14 +13,18 @@ using fakeit::When;
 
 namespace vva {
 
-class MockedOperationStrategyTest {
- protected:
-  fakeit::Mock<IOperationWarper> mockWarper_;
+struct MockedOperationStrategyTest {
+  std::unique_ptr<fakeit::Mock<IOperationWarper>> mockWarper_;
 };
 
-TEST_CASE_METHOD(MockedOperationStrategyTest,
-                 "MockOperationStrategy_Arg_Result",
-                 "[mock-operation-strategy]") {
+UTEST_F_SETUP(MockedOperationStrategyTest) {
+  utest_fixture->mockWarper_ = std::make_unique<fakeit::Mock<IOperationWarper>>();
+}
+
+UTEST_F_TEARDOWN(MockedOperationStrategyTest) {
+}
+
+UTEST_F(MockedOperationStrategyTest, MockOperationStrategy_Arg_Result) {
   constexpr int16_t first_arg = 6;
   constexpr int16_t second_arg = 2;
   constexpr int16_t operation_result = 4;
@@ -28,19 +33,19 @@ TEST_CASE_METHOD(MockedOperationStrategyTest,
   constexpr int16_t second_stage_result = 16;
   constexpr int16_t third_stage_result = 4;
 
-  When(Method(mockWarper_, addition).Using(first_arg, second_arg))
+  When(Method(*(utest_fixture->mockWarper_), addition).Using(first_arg, second_arg))
       .Return(first_stage_result);
-  When(Method(mockWarper_, multiplication).Using(first_stage_result, 2))
+  When(Method(*(utest_fixture->mockWarper_), multiplication).Using(first_stage_result, 2))
       .Return(second_stage_result);
-  When(Method(mockWarper_, subtraction).Using(first_arg, second_arg))
+  When(Method(*(utest_fixture->mockWarper_), subtraction).Using(first_arg, second_arg))
       .Return(third_stage_result);
-  When(Method(mockWarper_, division)
+  When(Method(*(utest_fixture->mockWarper_), division)
            .Using(second_stage_result, third_stage_result))
       .Return(operation_result);
 
-  OperationStrategy test_strategy(mockWarper_.get());
+  OperationStrategy test_strategy((utest_fixture->mockWarper_)->get());
 
-  REQUIRE(test_strategy(first_arg, second_arg) == operation_result);
+  EXPECT_EQ(test_strategy(first_arg, second_arg), operation_result);
 }
 
 }  // namespace vva
